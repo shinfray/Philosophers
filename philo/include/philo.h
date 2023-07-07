935684fe2949dd783f3e3012acb7f5bbd6116a9f
shinfray <simon@hinfray.eu>
2023-07-07 18:09:56 +0200

    can retrieve arguments

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: shinfray <shinfray@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 08:48:18 by shinfray          #+#    #+#             */
-/*   Updated: 2023/07/03 16:51:03 by shinfray         ###   ########.fr       */
+/*   Updated: 2023/07/07 17:52:09 by shinfray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,20 @@
 
 # include <pthread.h>
 # include <stdio.h>
+# include <inttypes.h>
+# include <stdbool.h>
+# include <unistd.h>
+
+typedef struct philo
+{
+	pthread_t		*philosophers;
+	pthread_mutex_t	*forks;
+	size_t			total_philosophers;
+	uint32_t		time_to_die;
+	uint32_t		time_to_eat;	
+	uint32_t		time_to_sleep;	
+	uint32_t		meal_goal;	
+	bool			infinite_mode;
+}				t_philo;
 
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,38 +6,83 @@
 /*   By: shinfray <shinfray@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 08:47:20 by shinfray          #+#    #+#             */
-/*   Updated: 2023/07/05 11:42:44 by shinfray         ###   ########.fr       */
+/*   Updated: 2023/07/07 18:09:15 by shinfray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	*routine(void *arg);
+// void	*routine(void *arg);
 
-int	mail = 0;
+// void	*routine(void *arg)
+// {
+// 	(void)arg;
+// 	return (NULL);
+// }
+uint32_t	ft_strtoul(const char *str, int *err_status);
+int	ft_set_philo(t_philo *philo, int argc, char **argv);
 
-void	*routine(void *arg)
+uint32_t	ft_strtoul(const char *str, int *err_status)
 {
-	int	i = 0;
-	(void)arg;
-	while (i++ < 5000000)
-		mail++;
-	return (NULL);
+	uint32_t	result;
+	uint32_t	copy;
+
+	*err_status = (*str == '\0');
+	if (*err_status != 0)
+		return (0);
+	result = 0;
+	while (*str >= '0' && *str <= '9')
+	{
+		copy = result;
+		result = result * 10 + (uint32_t)(*str++ - '0');
+		*err_status = (result < copy);
+		if (*err_status != 0)
+			return (0);
+	}
+	*err_status = (*str != '\0');
+	return (result);
 }
 
-int	main(void)
+int	ft_set_philo(t_philo *philo, int argc, char **argv)
 {
-	pthread_t	t1;
-	pthread_t	t2;
+	int	err_status;
+
+	err_status = 0;
+	philo->total_philosophers = ft_strtoul((const char *)argv[1], &err_status);
+	if (err_status != 0)
+		return (-1);
+	philo->time_to_die = ft_strtoul((const char *)argv[2], &err_status);
+	if (err_status != 0)
+		return (-1);
+	philo->time_to_eat = ft_strtoul((const char *)argv[3], &err_status);
+	if (err_status != 0)
+		return (-1);
+	philo->time_to_sleep = ft_strtoul((const char *)argv[4], &err_status);
+	if (err_status != 0)
+		return (-1);
+	philo->infinite_mode = (argc == 6);
+	if (argc == 6)
+		philo->meal_goal = ft_strtoul((const char *)argv[5], &err_status);
+	if (err_status != 0)
+		return (-1);
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_philo	s_philo;
 	
-	if (pthread_create(&t1, NULL, &routine, NULL) != 0)
+	(void)argv;
+	(void)s_philo;
+	if (argc < 5 || argc > 6)
+	{
+		write(2, "Wrong number of argumet\n", 24);
 		return (1);
-	if (pthread_create(&t2, NULL, &routine, NULL) != 0)
-		return (2);
-	if (pthread_join(t1, NULL) != 0)	
-		return (3);
-	if (pthread_join(t2, NULL) != 0)	
-		return (4);
-	printf("%d\n", mail);
+	}
+	ft_set_philo(&s_philo, argc, argv);
+	// if (pthread_create(&t1, NULL, &routine, NULL) != 0)
+	// 	return (1);
+	// if (pthread_join(t2, NULL) != 0)	
+	// 	return (4);
 	return (0);
 }
