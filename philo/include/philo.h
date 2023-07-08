81efe62f20f75613915d931f78904319b5fcef62
shinfray <simon@hinfray.eu>
2023-07-08 10:06:34 +0200

    allocate and initialize the program

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,17 +6,19 @@
 /*   By: shinfray <shinfray@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 08:48:18 by shinfray          #+#    #+#             */
-/*   Updated: 2023/07/07 17:52:09 by shinfray         ###   ########.fr       */
+/*   Updated: 2023/07/08 10:05:37 by shinfray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
+# include <inttypes.h>
 # include <pthread.h>
 # include <stdio.h>
-# include <inttypes.h>
+# include <stdlib.h>
 # include <stdbool.h>
+# include <string.h>
 # include <unistd.h>
 
 typedef struct philo
@@ -30,5 +32,25 @@ typedef struct philo
 	uint32_t		meal_goal;	
 	bool			infinite_mode;
 }				t_philo;
+
+/*
+ *		PARSING
+*/
+/*		PARSE FUNCTIONS			*/
+int		ft_check_arguments(int argc, char **argv, t_philo *philo);
+
+/*
+ *		INITIALIZATION
+*/
+/*		INITIALIZE FUNCTIONS	*/
+int		ft_initialize(t_philo *philo);
+
+/*
+ *		CLEANING
+*/
+/*		CLEAN FUNCTIONS			*/
+void	ft_clean(t_philo *philo);
+void	ft_destroy_mutexes(pthread_mutex_t *forks, size_t n);
+void	ft_free_all(t_philo *philo);
 
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: shinfray <shinfray@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 08:47:20 by shinfray          #+#    #+#             */
-/*   Updated: 2023/07/07 18:09:15 by shinfray         ###   ########.fr       */
+/*   Updated: 2023/07/08 10:05:44 by shinfray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,70 +19,19 @@
 // 	(void)arg;
 // 	return (NULL);
 // }
-uint32_t	ft_strtoul(const char *str, int *err_status);
-int	ft_set_philo(t_philo *philo, int argc, char **argv);
-
-uint32_t	ft_strtoul(const char *str, int *err_status)
-{
-	uint32_t	result;
-	uint32_t	copy;
-
-	*err_status = (*str == '\0');
-	if (*err_status != 0)
-		return (0);
-	result = 0;
-	while (*str >= '0' && *str <= '9')
-	{
-		copy = result;
-		result = result * 10 + (uint32_t)(*str++ - '0');
-		*err_status = (result < copy);
-		if (*err_status != 0)
-			return (0);
-	}
-	*err_status = (*str != '\0');
-	return (result);
-}
-
-int	ft_set_philo(t_philo *philo, int argc, char **argv)
-{
-	int	err_status;
-
-	err_status = 0;
-	philo->total_philosophers = ft_strtoul((const char *)argv[1], &err_status);
-	if (err_status != 0)
-		return (-1);
-	philo->time_to_die = ft_strtoul((const char *)argv[2], &err_status);
-	if (err_status != 0)
-		return (-1);
-	philo->time_to_eat = ft_strtoul((const char *)argv[3], &err_status);
-	if (err_status != 0)
-		return (-1);
-	philo->time_to_sleep = ft_strtoul((const char *)argv[4], &err_status);
-	if (err_status != 0)
-		return (-1);
-	philo->infinite_mode = (argc == 6);
-	if (argc == 6)
-		philo->meal_goal = ft_strtoul((const char *)argv[5], &err_status);
-	if (err_status != 0)
-		return (-1);
-	return (0);
-}
 
 int	main(int argc, char **argv)
 {
 	t_philo	s_philo;
 	
-	(void)argv;
-	(void)s_philo;
-	if (argc < 5 || argc > 6)
-	{
-		write(2, "Wrong number of argumet\n", 24);
+	if (ft_check_arguments(argc, argv, &s_philo) == -1)
 		return (1);
-	}
-	ft_set_philo(&s_philo, argc, argv);
+	if (ft_initialize(&s_philo) == -1)
+		return (2);
 	// if (pthread_create(&t1, NULL, &routine, NULL) != 0)
 	// 	return (1);
 	// if (pthread_join(t2, NULL) != 0)	
 	// 	return (4);
+	ft_clean(&s_philo);
 	return (0);
 }
