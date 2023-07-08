e8db1c309b05894cf66f4077e9df118003f1e938
shinfray <simon@hinfray.eu>
2023-07-08 10:12:43 +0200

    formating header

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: shinfray <shinfray@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 08:48:18 by shinfray          #+#    #+#             */
-/*   Updated: 2023/07/08 10:05:37 by shinfray         ###   ########.fr       */
+/*   Updated: 2023/07/08 10:11:30 by shinfray         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,22 +33,19 @@ typedef struct philo
 	bool			infinite_mode;
 }				t_philo;
 
-/*
- *		PARSING
-*/
-/*		PARSE FUNCTIONS			*/
+/* ************************************************************************** */
+/*		PARSING                                                               */
+/* ************************************************************************** */
 int		ft_check_arguments(int argc, char **argv, t_philo *philo);
 
-/*
- *		INITIALIZATION
-*/
-/*		INITIALIZE FUNCTIONS	*/
+/* ************************************************************************** */
+/*		INITIALIXATION                                                        */
+/* ************************************************************************** */
 int		ft_initialize(t_philo *philo);
 
-/*
- *		CLEANING
-*/
-/*		CLEAN FUNCTIONS			*/
+/* ************************************************************************** */
+/*		CLEANING                                                              */
+/* ************************************************************************** */
 void	ft_clean(t_philo *philo);
 void	ft_destroy_mutexes(pthread_mutex_t *forks, size_t n);
 void	ft_free_all(t_philo *philo);
